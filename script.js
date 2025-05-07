6f15c8da71b08fd0c17b54a5968009c9ce194890
Manish <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-07 08:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if(year % 4 == 0){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+year = prompt('Enter Yeare');
+a = daysOfAYear(year);
+console.log(a);
